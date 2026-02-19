58405e364b00ac62166be63dab45242679f491ae
deepthinatarajan05-web <deepthinatarajan05@gmail.com>
2026-02-19 12:07:07 +0530

    Delete myproject directory

--- myproject/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class Student(models.Model):
-     name=models.CharField(max_length=30)
-     age=models.IntegerField()
-     course=models.CharField(max_length=30)
-def __str__(self):
-    return f"{self.name} ({self.course})"
-# Create your models here.
